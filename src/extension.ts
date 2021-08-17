7d9b04bb78df0590d686af79557b716133119c36
yoga-au <yogaagung.utama@gmail.com>
2021-08-17 16:30:40 +0700

    add convert funtion

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,59 @@
 import * as vscode from "vscode";
+import { builtRange, isPx, convertToEm } from "./utils";
 
 // TODO:
-// - conversion to em/rem
 // - add handler when range undefined
+// - extract value from selection
+// - conversion to em/rem
+
+// RESEARCH:
+// checking if value ends with certain unit
+// const str1 = '24px';
+// console.log(str1.endsWith('px'));
+// ----
+// extract number from value
+// const str = "16px";
+// console.log(str.indexOf("px"));
+// console.log(str.slice(0, str.indexOf('px')));
 
 const textEditor = vscode.window.activeTextEditor;
+const infoMessage = vscode.window.showInformationMessage;
+const errorMessage = vscode.window.showErrorMessage;
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "px-to-em" is now active!');
 
   // command list
-  const pxToEm = "px-to-em.pxToEm";
+  const pxToEmCmd = "px-to-em.pxToEm";
 
-  const disposable = vscode.commands.registerCommand(pxToEm, () => {
-    // console.log(textEditor?.selection.start, textEditor?.selection.end);
-    // const range = new vscode.Range(
-    //   textEditor?.selection.start!,
-    //   textEditor?.selection.end!
-    // );
+  const pxToEm = vscode.commands.registerCommand(pxToEmCmd, () => {
     if (textEditor?.selection.start && textEditor.selection.end) {
-      const range = new vscode.Range(
+      const range = builtRange(
         textEditor.selection.start,
         textEditor.selection.end
       );
 
-      console.log(textEditor?.document.getText(range));
-      vscode.window.showInformationMessage("Hello World from px-to-em!");
+      const selectionValue = textEditor.document.getText(range);
+
+      // if no selection or selection are empty
+      if (selectionValue === "") {
+        return errorMessage("Empty String");
+      }
+
+      // check if its end with px
+      if (!isPx(selectionValue)) {
+        return errorMessage("The selection is not a pixel value");
+      }
+
+      console.log(`${convertToEm(selectionValue)}em`);
+      infoMessage("Sucess");
       return;
     }
 
-    vscode.window.showErrorMessage("Error");
+    errorMessage("Error");
   });
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(pxToEm);
 }
 
 export function deactivate() {}
---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+import * as vscode from "vscode";
+
+// CHANGE THIS to vscode configuration contirbution point
+const basePixel = 16;
+
+export const builtRange = (start: vscode.Position, end: vscode.Position) => {
+  return new vscode.Range(start, end);
+};
+
+export const isPx = (value: string) => {
+  return value.endsWith("px");
+};
+
+export const convertToEm = (value: string) => {
+  // extract value
+  const indexOfPx = value.indexOf("px");
+  const numValue = parseInt(value.slice(0, indexOfPx));
+
+  // conversion to em
+  const convertResult = numValue / basePixel;
+  return convertResult;
+};
