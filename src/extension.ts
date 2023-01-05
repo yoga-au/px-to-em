cd51b6b653d049c936d7ebdcaecbd48580f0c70a
Yoga Agung Utama <yogaagung.utama@gmail.com>
2023-01-05 13:54:35 +0700

    Revert "add: code actions for px em"
    This reverts commit 12c6a02ba32437336076a9d6bc5d4167629932df.

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import { commands, languages, workspace, window } from "vscode";
+import { commands, ExtensionContext } from "vscode";
 import {
   pxToEm,
   emToPx,
@@ -6,56 +6,14 @@ import {
   remToPx,
   changeRootFont,
 } from "./commands/index";
-import { ConvertPxEm } from "./actions/convertPxEm";
-
-import type { ExtensionContext, ConfigurationChangeEvent } from "vscode";
-
-const showRestartMessage = async ({
-  affectsConfiguration,
-}: ConfigurationChangeEvent) => {
-  if (affectsConfiguration("pxToEm.codeAction.enabled")) {
-    window
-      .showWarningMessage(
-        "Reloading VS Code is required to apply changes",
-        "Reload",
-        "Ignore"
-      )
-      .then((action) => {
-        if (action && action === "Reload") {
-          commands.executeCommand("workbench.action.reloadWindow");
-        }
-      });
-  }
-};
-
-// ! PROBLEM: the code actions only activate when css file is selected
-// TODO: figure out how to activate code actions dynamically/in all files
 
 export function activate(context: ExtensionContext) {
-  const codeActionConfig = workspace
-    .getConfiguration("pxToEm")
-    .get<string[]>("codeAction.enabled", ["css"]);
-
-  workspace.onDidChangeConfiguration((e) => showRestartMessage(e));
-
   context.subscriptions.push(
     commands.registerCommand("px-to-em.pxToEm", () => pxToEm()),
     commands.registerCommand("px-to-em.emToPx", () => emToPx()),
     commands.registerCommand("px-to-em.pxToRem", () => pxToRem()),
     commands.registerCommand("px-to-em.remToPx", () => remToPx()),
-    commands.registerCommand("px-to-em.changeRootFont", () => changeRootFont()),
-    languages.registerCodeActionsProvider(
-      codeActionConfig,
-      new ConvertPxEm(
-        "px-to-em.pxToEm",
-        "px-to-em.emToPx",
-        "px-to-em.pxToRem",
-        "px-to-em.remToPx"
-      ),
-      {
-        providedCodeActionKinds: ConvertPxEm.providedCodeActionKinds,
-      }
-    )
+    commands.registerCommand("px-to-em.changeRootFont", () => changeRootFont())
   );
 }
 
